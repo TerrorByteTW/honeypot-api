ef6d4ba7eb995b5fd8177dad130b692a1b905f7a
Nate Reprogle <nate.reprogle@gmail.com>
2022-08-08 22:12:03 -0500

    Updated API to include new management classes

---
+++ src/main/java/org/reprogle/honeypot/storagemanager/HoneypotPlayerHistoryManager.java
@@ -0,0 +1,43 @@
+package org.reprogle.honeypot.storagemanager;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+public class HoneypotPlayerHistoryManager {
+
+    /**
+     * Add an entry to the player history table
+     * @param p The player to add
+     * @param b The honeypot block they triggered
+     */
+    public void addPlayerHistory(Player p, HoneypotBlockObject b) {
+        // Intentionally left blank
+    }
+
+    /**
+     * Get the history for a player
+     * @param p The player to grab history for
+     * @return A list of all HoneypotPlayerHistory objects
+     */
+    public List<HoneypotPlayerHistoryObject> getPlayerHistory(Player p) {
+        return getPlayerHistory(p);
+    }
+
+    /**
+     * Delete all history for a particular player. An optional n parameter for specifying the number of most recent rows to delete
+     * @param p The player to delete
+     * @param n Optional, the number of most recent rows
+     */
+    public void deletePlayerHistory (Player p, int... n) {
+        // Intentionally left blank
+    }
+
+    /**
+     * A function to purge the entire history table
+     */
+    public void deleteAllHistory() {
+        // Intentionally left blank
+    }
+
+}
---
+++ src/main/java/org/reprogle/honeypot/storagemanager/HoneypotPlayerHistoryObject.java
@@ -0,0 +1,63 @@
+package org.reprogle.honeypot.storagemanager;
+
+import org.bukkit.entity.Player;
+
+@SuppressWarnings({"unused", "java:S1068", "java:S116"})
+public abstract class HoneypotPlayerHistoryObject {
+    private String dateTime;
+    private String player;
+    private String UUID;
+    private HoneypotBlockObject hbo;
+
+    /**
+     * Constructor for creating a history entry
+     * @param dateTime The Date and Time in string format. Really need to improve this to have standards but oh well
+     * @param player The player's name
+     * @param UUID The UUID of the player
+     * @param hbo The HoneypotBlockObject they broke
+     */
+    protected HoneypotPlayerHistoryObject(String dateTime, String player, String UUID, HoneypotBlockObject hbo) {
+        this.dateTime = dateTime;
+        this.player = player;
+        this.UUID = UUID;
+        this.hbo = hbo;
+    }
+
+    /**
+     * Constructor for creating a history entry
+     * @param dateTime The Date and Time in string format. Really need to improve this to have standards but oh well
+     * @param player The player object
+     * @param hbo The HoneypotBlockObject they broke
+     */
+    protected HoneypotPlayerHistoryObject(String dateTime, Player player, HoneypotBlockObject hbo) {
+        this.dateTime = dateTime;
+        this.player = player.getName();
+        this.UUID = player.getUniqueId().toString();
+        this.hbo = hbo;
+    }
+
+    /**
+     * Get the date and time of the history entry
+     * @return Date and Time, string formatted and in GMT +0:00
+     */
+    public abstract String getDateTime();
+
+    /**
+     * Get the player's name
+     * @return Player name
+     */
+    public abstract String getPlayer();
+
+    /**
+     * Get the player's UUID
+     * @return UUID
+     */
+    public abstract String getUUID();
+
+    /** Get the Honeypot they broke
+     * @return {@link HoneypotBlockObject}
+     */
+    public abstract HoneypotBlockObject getHoneypot();
+
+    
+}
